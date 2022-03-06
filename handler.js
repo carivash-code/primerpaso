13cf4815fc0854d10d86270890fc29effb6647ef
carivash <carivash.dev@gmail.com>
2022-03-06 02:51:46 -0600

    [add] rest API logic

--- handler.js
+++ handler.js
@@ -5,14 +5,26 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Hola mundo desde Serverless',
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.helloUser = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hola ${event.pathParameters.name}`,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
