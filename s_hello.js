948f3003c5fcbf0306c93419628079706599deab
carivash <carivash.dev@gmail.com>
2022-03-08 23:01:09 -0600

    create API call GET & POST

---
+++ s_hello.js
@@ -0,0 +1,29 @@
+
+var serverlessSDK = require('./serverless_sdk/index.js');
+serverlessSDK = new serverlessSDK({
+  orgId: 'carivash',
+  applicationName: 'curso-sls-hola-mundo',
+  appUid: 'c8fkLhQsJlpGv547Nv',
+  orgUid: 'df80c303-2a54-4670-b3b6-e2e4de88fc39',
+  deploymentUid: '8dac46f1-5bd6-46e6-b595-6ba88377df44',
+  serviceName: 'curso-sls-hola-mundo',
+  shouldLogMeta: true,
+  shouldCompressLogs: true,
+  disableAwsSpans: false,
+  disableHttpSpans: false,
+  stageName: 'dev',
+  serverlessPlatformStage: 'prod',
+  devModeEnabled: false,
+  accessKey: null,
+  pluginVersion: '6.1.5',
+  disableFrameworksInstrumentation: false
+});
+
+const handlerWrapperArgs = { functionName: 'curso-sls-hola-mundo-dev-hello', timeout: 6 };
+
+try {
+  const userHandler = require('./handler.js');
+  module.exports.handler = serverlessSDK.handler(userHandler.hello, handlerWrapperArgs);
+} catch (error) {
+  module.exports.handler = serverlessSDK.handler(() => { throw error }, handlerWrapperArgs);
+}
